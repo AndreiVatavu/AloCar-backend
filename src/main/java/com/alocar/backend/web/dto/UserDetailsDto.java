e3e81b96b611b634698c7e291c3b0d68accab252
spbogdan <sp_bogdan@yahoo.ro>
2019-05-20 01:44:14 +0300

    Field validation messages

--- src/main/java/com/alocar/backend/web/dto/UserDetailsDto.java
+++ src/main/java/com/alocar/backend/web/dto/UserDetailsDto.java
@@ -2,6 +2,7 @@ package com.alocar.backend.web.dto;
 
 import com.alocar.backend.validation.ValidEmail;
 import com.alocar.backend.validation.ValidPhoneNumber;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -9,14 +10,16 @@ import javax.validation.constraints.Size;
 /**
  * Created by Andrei Vatavu on 4/23/2019
  */
+
+@PropertySource("classpath:messages.properties")
 public class UserDetailsDto {
 
     @NotNull
-    @Size(min = 1)
+    @Size(min = 1, message = "{Invalid.firstName.size}")
     private String firstName;
 
     @NotNull
-    @Size(min = 1)
+    @Size(min = 1, message = "Invalid last name")
     private String lastName;
 
     @NotNull
@@ -28,7 +31,7 @@ public class UserDetailsDto {
     private String phoneNumber;
 
     @NotNull
-    @Size(min = 1)
+    @Size(min = 1, message = "Invalid password")
     private String password;
 
     public UserDetailsDto() {
